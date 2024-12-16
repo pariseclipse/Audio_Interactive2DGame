4413ad28f3dc9bd46236f634cc8bab8a5e63f555
pariseclipse <104399068+pariseclipse@users.noreply.github.com>
2024-12-16 10:28:46 +0000

    Fixing the light time interval

--- lights_container.gd
+++ lights_container.gd
@@ -11,16 +11,28 @@ extends Node2D
 @export var min_brightness = 0.5
 @export var max_brightness = 1.0
 
+@export var flash_interval = 0.5 # Time in seconds between flashes
+
 @onready var lights = get_children()  # Get all child nodes under LightsContainer
+var time_since_last_flash = 0.0  # Timer to track time between flashes
 
 func _ready():
 	randomize()  # Ensure random values are unique on each run
 
 func _process(delta):
-	for light in lights:
-		if light is PointLight2D:
-			# Randomly change the color
-			light.color = colors[randi() % colors.size()]
-			
-			# Randomly change the brightness
-			light.energy = lerp(min_brightness, max_brightness, randf())
+	# Increment the timer
+	time_since_last_flash += delta
+	
+	# Check if enough time has passed for the next flash
+	if time_since_last_flash >= flash_interval:
+		# Update lights
+		for light in lights:
+			if light is PointLight2D:
+				# Randomly change the color
+				light.color = colors[randi() % colors.size()]
+				
+				# Randomly change the brightness
+				light.energy = lerp(min_brightness, max_brightness, randf())
+		
+		# Reset the timer
+		time_since_last_flash = 0.0
